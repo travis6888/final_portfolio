de298d0e17a07113db4ac1ac61558c221078f621
travis6888 <travis6888@yahoo.com>
2015-02-15 15:55:12 -0800

    heroku

--- formal_portfolio/wsgi.py
+++ formal_portfolio/wsgi.py
@@ -14,5 +14,7 @@ from django.core.wsgi import get_wsgi_application
 # application = get_wsgi_application()
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
