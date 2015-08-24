70e9f36707fc6aa264386fa53c17d76faade2627
travis6888 <travis6888@yahoo.com>
2015-08-23 17:20:49 -0700

    aws

---
+++ portfolio_main/management/commands/__init__.py
@@ -0,0 +1,1 @@
+__author__ = 'travis6888'
---
+++ portfolio_main/management/commands/createsu.py
@@ -0,0 +1,13 @@
+__author__ = 'travis6888'
+
+
+import os
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("travis", "travis@traviscockcroft.com", "travis88")
